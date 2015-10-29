c8cfcee12649e3510c5293b954d8057adeb43c38
Einar Holsbø <einar@cs.uit.no>
2015-10-29 15:02:38 +0100

    Bootstrap resampling

--- R/utility.R
+++ R/utility.R
@@ -17,3 +17,33 @@ glm_rank <- function(x, y, family=gaussian) {
 
   return(pvals)
 }
+
+#' Draw bootstrap samples
+#'
+#' Draws n samples in the range \code{1:n} with replacement. Provides the
+#' out-of-bag samples for testing.
+#'
+#' @param n The size of the data set to draw samples from
+#' @param b Number of bootstrap resamples to draw
+#' @return Returns an object of class "bootsample" with the following components
+#' \item{samples}{a \code{b} by \code{n} matrix containing bootstrap samples}
+#' \item{oob}{a list of b vectors that provide the samples that arent in the
+#'  respective bootstrap samples}
+#' @export
+#' @examples
+#' data("iris")
+#' nsamples <- nrow(iris)
+#' bs <- bootsample(nsamples, 1)  # draw a single bootstrap sample
+#'
+#' ## split into training and test sets based on the bootstrap resampling
+#' train <- iris[bs$sample[1, ], ]
+#' test <- iris[bs$oob[[1]], ]
+bootsample <- function(n, b=1000) {
+  samples <- t(replicate(b, sample(1:n, n, replace=TRUE)))
+  oob <- alply(samples, 1, function(x) { which(!(1:n %in% x)) })
+
+  bs <- list(samples=samples, oob=oob)
+  class(bs) <- "bootsample"
+
+  bs
+}
