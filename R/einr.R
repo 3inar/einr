15d279a7d940558480c2c88aa0cf36025e439028
Einar Holsbø <einar@cs.uit.no>
2015-11-04 16:24:24 +0100

    Add v. unhelpful package documentation

---
+++ R/einr.R
@@ -0,0 +1,10 @@
+#' einr: a collection of functions &c.
+#'
+#' This package collects functions that I have implemented either because I
+#' keep needing the same thing over and over or because I was curious about
+#' some method that I couldn't find implemented elsewhere. Or because I for some
+#' reason wanted to implement it myself even though it was already out there.
+#'
+#' @docType package
+#' @name einr
+NULL
