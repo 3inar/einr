49469a8d66c141dab167dc0f558ee6b4c18a28e3
Einar Holsbø <einar@cs.uit.no>
2016-06-21 10:56:44 +0200

    New & better aggregate functions

--- R/rdaggregate.R
+++ R/rdaggregate.R
@@ -2,27 +2,43 @@
 #'
 #' Read all the files in a directory ending in ".rda" and combine their contents
 #' by either \code{cbind} or \code{rbind}. Assumes each \code{.rda} file contains
-#' a single object.
+#' a single object. Assumes array inputs, produces array outputs. Combines by row.
 #'
 #' @param directory path to the directory to search.
-#' @param byrow combine by row or by column. Defaults to \code{TRUE}.
 #'
-#' @return a matrix or data frame containing the contents of the \code{.rda} files
+#' @return a matrix containing the contents of the \code{.rda} files
 #'  in \code{directory}.
 #'
 #' @export
-# TODO: add @example
-rdaggregate <- function(directory, byrow=T) {
+aaggr <- function(directory) {
   histobj <- NULL
   files <- list.files(path = directory, pattern = "*.rda")
   for (f in files) {
     objectname <- load(file.path(directory, f))
+    histobj <- rbind(histobj, get(objectname))
+  }
 
-    if (byrow) {
-      histobj <- rbind(histobj, get(objectname))
-    } else {
-      histobj <- cbind(histobj, get(objectname))
-    }
+  histobj
+}
+
+#' Aggregate the \code{.rda} files in a given directory
+#'
+#' Read all the files in a directory ending in ".rda" and combine their contents
+#' by either \code{cbind} or \code{rbind}. Assumes each \code{.rda} file contains
+#' a single object. Assumes list inputs, produces list outputs. Combines by row.
+#'
+#' @param directory path to the directory to search.
+#'
+#' @return list containing the contents of the \code{.rda} files
+#'  in \code{directory}.
+#'
+#' @export
+llggr <- function(directory) {
+    histobj <- NULL
+  files <- list.files(path = directory, pattern = "*.rda")
+  for (f in files) {
+    objectname <- load(file.path(directory, f))
+    histobj <- c(histobj, get(objectname))
   }
 
   histobj
